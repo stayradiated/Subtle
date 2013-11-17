7713f98e24a521fa64a8e16da2c99517a0fc8ff7
George Czabania <george@czabania.com>
2013-11-17 14:13:41 +1300

    start adding in scaling

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
 
 (function () {
 
-  var canvas = document.getElementById('wallaper');
-  var ctx = canvas.getContext('2d');
-  var image = new Image();
+  var init, scaleImage;
 
-  image.onload = function () {
-    ctx.drawImage(image, 0, 0, 100, 100);
+  document.addEventListener('DOMContentLoaded', function () {
+    document.removeEventListener('DOMContentLoaded', arguments.callee, false);
+    init();
+  });
+
+  init = function () {
+
+    var canvas = document.getElementById('wallpaper');
+    var ctx = canvas.getContext('2d');
+    var image = new Image();
+
+    image.onload = function () {
+      scaleImage(image, {width: 1920, height: 1080});
+      // ctx.drawImage(image, 0, 0, 1920, 1080);
+    };
+
+    image.src = 'test.jpg';
+
   };
 
-  image.src = 'test.jpg';
+  /**
+   * Resize and crop an Image to fit a specific size
+   * - image (Image) : An Image object
+   * - size (Object) : Contains the destination width and height
+   * > array : the arguments for drawImage
+   */
+  scaleImage = function(image, size) {
+
+    // The original dimensions of the image
+    var original = {
+      width: image.width,
+      height: image.height
+    };
+
+    var ratio = size.width / size.height;
+
+    console.log(ratio);
+
+  };
 
 }());
 
